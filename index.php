52279d2496b3c91de89a357f22293360793051f2
Artem Siliuk <asiliuk@mindk.com>
2017-01-16 10:21:19 +0200

    fix(config): added parameters exception handling to index.php

--- index.php
+++ index.php
@@ -1,16 +1,18 @@
 <?php
 
-require_once("ConfigSingleton.php");
+require_once "ConfigSingleton.php";
 require_once "DatabaseAdapterFactory.php";
 require_once "DatabaseAdapters/DatabaseAdapters.php";
 require_once "Exceptions/IncorrectAdapterNameException.php";
-
-$config = ConfigSingleton::getInstance();
+require_once "Exceptions/ParametersParseException.php";
 
 try {
+    $config = ConfigSingleton::getInstance();
     $database_factory = new DatabaseAdapterFactory();
     $adapter = $database_factory->getAdapter($config->get("database")["adapter"]);
     debug($adapter);
+} catch (ParametersParseException $e) {
+    echo $e->getMessage();
 } catch (IncorrectAdapterNameException $e) {
     echo $e->getMessage();
 }
